b3f4ce8215be19269e563c84045d5516de9251dd
François Cordier <FrancoisCordier@users.noreply.github.com>
2021-01-21 12:20:45 +0100

    Added controllers

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const knex = require("knex")({
     database: "face_recognition",
   },
 });
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
 const app = express();
 app.use(bodyParser.json());
@@ -22,87 +26,19 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-  knex
-    .select("email", "hash")
-    .from("login")
-    .where("email", "=", req.body.email)
-    .then((data) => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        knex
-          .select("*")
-          .from("users")
-          .where("email", "=", req.body.email)
-          .then((user) => {
-            res.json(user[0]);
-          })
-          .catch((err) => res.status(400).json("Unable to get user"));
-      } else {
-        res.status(400).json("Wrong credentials");
-      }
-    })
-    .catch((err) => res.status(400).json("Wrong credentials"));
+  signin.handleSignin(req, res, knex, bcrypt);
 });
 
 app.post("/register", (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-  knex.transaction((trx) => {
-    trx
-      .insert({
-        hash: hash,
-        email: email,
-      })
-      .into("login")
-      .returning("email")
-      .then((loginEmail) => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            email: loginEmail[0],
-            name: name,
-            joined: new Date(),
-          })
-          .then((user) => {
-            res.json(user[0]);
-          })
-          .then(trx.commit)
-          .catch(trx.tollback);
-      })
-      .catch((err) => res.status(400).json("Unable to register"));
-  });
+  register.handleRegister(req, res, knex, bcrypt);
 });
 
 app.get("/profile/:id", (req, res) => {
-  const { id } = req.params;
-
-  knex
-    .select("*")
-    .from("users")
-    .where({
-      id: id,
-    })
-    .then((user) => {
-      if (user.length) {
-        res.json(user[0]);
-      } else {
-        res.status(400).json("Not found");
-      }
-    })
-    .catch((err) => res.status(400).json("Error getting user"));
+  profile.handleGetProfile(req, res, knex);
 });
 
 app.put("/image", (req, res) => {
-  const { id } = req.body;
-
-  knex("users")
-    .where("id", "=", id)
-    .increment("entries", 1)
-    .returning("entries")
-    .then((entries) => {
-      res.json(entries[0]);
-    })
-    .catch((err) => res.status(400).json("Unable to get entries"));
+  image.handleImage(req, res, knex);
 });
 
 app.listen(3000);
